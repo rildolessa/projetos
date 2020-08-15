a561f117d0e9931f9a625e95eb849408b42ab8ee
rildolessa <rildolessa@gmail.com>
2020-08-15 12:53:26 -0300

    Aula spring boot

---
+++ Aulas/java/springBoot/src/main/java/br/com/netbyweb/springBoot/Tipos.java
@@ -0,0 +1,16 @@
+package br.com.netbyweb.springBoot;
+
+public class Tipos {
+
+	public static void main(String[] args) {
+
+		Byte meuByte = null;
+		long meuLong = 0;
+		boolean verdadeiro = true;
+		int numero = new Integer(3);
+		
+		System.out.println(numero);
+
+	}
+
+}
